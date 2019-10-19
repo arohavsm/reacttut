ae257a254c3e9956c72a2cdf088edc87e98d80bd
pamuleti <pamuleti@aroha.co.in-at-613112965612>
2019-10-19 22:30:45 +0530

    Start Practicing

--- react-crud-app/src/App.js
+++ react-crud-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Let Us Practice Here
         </a>
       </header>
     </div>
